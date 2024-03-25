97c9fb1e4b2034671e5b764f40fb9081e236ed60
jmook <jwonmook@gmail.com>
2024-03-25 20:32:08 +0900

    🐛 러스트 종료 메세지 추가

--- native/hub/src/lib.rs
+++ native/hub/src/lib.rs
@@ -1,7 +1,7 @@
-#[cfg(target_os = "windows")]
-use std::os::windows::thread;
-#[cfg(target_os = "macos")]
-use std::os::unix::thread;
+// #[cfg(target_os = "windows")]
+// use std::os::windows::thread;
+// #[cfg(target_os = "macos")]
+// use std::os::unix::thread;
 
 use std::sync::{Arc, Mutex};
 use tokio_with_wasm::tokio;
@@ -73,8 +73,8 @@ pub async fn stream_mraw_image(){
             total_idx: 1,
         }));
     
-    let a_playstate2 = Arc::clone(&a_playstate);
-    let a_mrawinfo2 = Arc::clone(&a_mrawinfo);
+    // let a_playstate2 = Arc::clone(&a_playstate);
+    // let a_mrawinfo2 = Arc::clone(&a_mrawinfo);
     let a_playstate3 = Arc::clone(&a_playstate);
     let a_mrawinfo3 = Arc::clone(&a_mrawinfo);
     let a_playstate4 = Arc::clone(&a_playstate);
@@ -125,6 +125,9 @@ pub async fn stream_mraw_image(){
             let msg = dart_signal.message;
             let _cmd = msg.cmd;
 
+            if _cmd == "Exit" {
+                std::process::exit(1);
+            }
             if _cmd == "Play" {
                 state.play = true;
             }
@@ -140,11 +143,11 @@ pub async fn stream_mraw_image(){
             }
             if _cmd == "Inc" {
                 state.current_idx += 1;
-                if (state.current_idx >= state.total_idx - 1){ state.current_idx = 0; }
+                if state.current_idx >= state.total_idx - 1 { state.current_idx = 0; }
             }
             if _cmd == "Dec" {
                 state.current_idx -= 1;
-                if (state.current_idx <= 0){  state.current_idx = state.total_idx.clone() - 1; }
+                if state.current_idx <= 0 {  state.current_idx = state.total_idx.clone() - 1; }
             }
             if _cmd == "Close" {
                 info.filepath = "".to_string();
@@ -159,6 +162,10 @@ pub async fn stream_mraw_image(){
                 state.play = false;
                 state.current_idx = 0;
                 state.total_idx = 1;
+            }
+            if _cmd == "Encoding" {
+                let framedata_arc_clone = Arc::clone(&info.framedata);
+                make_mp4(&framedata_arc_clone, info.width, info.height);
             }
             println!("recv cmd : {}", _cmd);
         }
@@ -183,7 +190,7 @@ pub async fn stream_mraw_image(){
                 let frameimage = load_frame(&framedata_arc_clone, _width, _height, _state.current_idx);
 
                 let sss = start.elapsed().as_millis() as u64;
-                if (sss < 33) { std::thread::sleep(Duration::from_millis(33 - sss)); }
+                if sss < 33 { std::thread::sleep(Duration::from_millis(33 - sss)); }
 
                 MessageRaw { 
                     height: _height,
@@ -215,11 +222,10 @@ fn load_image_file(filepath: String, width: u32, height: u32) -> Option<Vec<Vec<
     let mut image_data: Vec<Vec<u16>> = Vec::new();
     let frame_size = (width * height) as usize;
 
-    if let Ok(file) = File::open(filepath.clone()) {
+    if let Ok(_) = File::open(filepath.clone()) {
 
         let bytes: Vec<u8> = std::fs::read(filepath).unwrap();
         let mut frame: Vec<u16> = Vec::with_capacity(frame_size);
-        let mut idx = 0;
     
         for byte_pair in bytes.chunks_exact(2) {
             // let short = u16::from_le_bytes([byte_pair[0], byte_pair[1]]);
@@ -407,4 +413,192 @@ pub fn draw_image(r_value: i32, g_value: i32, b_value: i32, height: u32, width: 
         Ok(_) => Some(image_data),
         Err(_) => None,
     }
+}
+
+pub fn make_mp4(framebuf:&Vec<Vec<u16>>, width: u32, height: u32) {
+    
+    // use image::{GrayImage, Luma};
+    // use std::path::Path;
+    // for (index, frame) in framebuf.into_iter().enumerate() {
+    //     // Vec<u16>를 GrayImage로 변환
+    //     let img = GrayImage::from_fn(width, height, |x, y| {
+    //         let pixel_index = (y * width + x) as usize;
+    //         Luma([frame[pixel_index] as u8])
+    //     });
+
+    //     // 이미지 파일로 저장
+    //     let file_name = format!("C:\\frames\\framesss_{:03}.png", index);
+    //     let _res = img.save(Path::new(&file_name));
+    //     match _res {
+    //         Ok(_) => print!("saved {}\n", file_name),
+    //         Err(_) => print!("failed {}\n", file_name)
+    //     }
+    // }
+
+    // Ok(())
+    
+
+    /////////////////////////////////////////////////////////////////////////////
+
+
+    // extern crate ffmpeg_next as ffmpeg;
+    
+    // use ffmpeg::{
+    //     codec, format, util::frame::video::Video,
+    //     // codec, format, media, packet, software::scaling::{Context, flag::Flags}, util::frame::video::Video,
+    // };
+
+    // let _ = ffmpeg::init();
+
+    // let mut octx = format::output(&"C:\\output.mp4").unwrap();
+    // // let codec = ffmpeg::codec::encoder::find_encoder_by_name("libx264").ok_or("Codec not found");
+    // let codec = codec::encoder::find(codec::Id::H264);
+    
+    // let mut ost = octx.add_stream(codec).unwrap();
+    
+    // // let mut encoder = codec::encoder::video::Video::from_parameters(ost.parameters())?;
+    // let mut vencoder = codec::context::Context::from_parameters(ost.parameters()).unwrap().encoder().video().unwrap();
+    // vencoder.set_height(height);
+    // vencoder.set_width(width);
+    // vencoder.set_format(ffmpeg_next::util::format::pixel::Pixel::RGB24);
+    // ost.set_parameters(&vencoder);
+    // // vencoder.open();
+    
+    // // let mut scaler = Context::get(
+    // //     // codec::context::input(&encoder),
+    // //     // codec::context::output(&encoder),
+    // //     // Flags::BILINEAR,
+    // //     vencoder.format(),
+    // //     vencoder.width(),
+    // //     vencoder.height(),
+    // //     ffmpeg_next::util::format::pixel::Pixel::RGB24,
+    // //     vencoder.width(),
+    // //     vencoder.height(),
+    // //     Flags::BILINEAR,
+    // // ).unwrap();
+
+    // let mut frame = Video::empty();
+    // frame.set_format(ffmpeg_next::util::format::pixel::Pixel::RGB24);
+    // frame.set_width(width);
+    // frame.set_height(height);
+
+    // for (i, frame_data) in framebuf.iter().enumerate() {
+
+    //     frame.set_pts(Some(i as i64));
+
+    //     let mut rgb_frame = Video::empty();
+    //     rgb_frame.set_format(ffmpeg::util::format::pixel::Pixel::RGB24);
+    //     rgb_frame.set_width(width);
+    //     rgb_frame.set_height(height);
+
+    //     for (idx, pixel) in frame_data.iter().enumerate() {
+    //         let gray_value = (pixel / 64) as u8;
+
+    //         rgb_frame.data_mut(0)[idx * 3 + 0] = gray_value; // R
+    //         rgb_frame.data_mut(0)[idx * 3 + 1] = gray_value; // G
+    //         rgb_frame.data_mut(0)[idx * 3 + 2] = gray_value; // B
+    //     }
+        
+    //     if vencoder.send_frame(&rgb_frame).is_ok() {
+    //         let mut encoded = ffmpeg::Packet::empty();
+    //         while let Ok(_) = vencoder.receive_packet(&mut encoded) {
+    //             // octx.write_frame(&encoded).unwrap();
+    //             encoded.write_interleaved(&mut octx).unwrap();
+    //         }
+    //     }
+    // }
+
+    // // encoder.send_eof();
+    // // while let Ok(packet) = encoder.receive_packet() {
+    // //     octx.write_frame(&packet)?;
+    // // }
+
+    // // octx.write_trailer();
+
+    // // vencoder.send_eof();
+    // // let mut encoded = ffmpeg::Packet::empty();
+    // // while let Ok(_) = vencoder.receive_packet(&mut encoded) {
+    // //     encoded.write_interleaved(&mut octx).unwrap();
+    // // }
+
+    // octx.write_trailer().unwrap();
+
+    // // Ok(())
+
+
+    /////////////////////////////////////////////////////////////////////////////
+
+
+    // use video_rs::encode::{Encoder, Settings};
+    // use video_rs::time::Time;
+    // use std::path::Path;
+    // extern crate ffmpeg_next as ffmpeg;
+    // use ffmpeg::{
+    //     codec, format, media, packet, software::scaling::{Context, flag::Flags}, util::frame::video::Video,
+    // };
+
+    // // video_rs::init().unwrap();
+
+    // let settings = Settings::preset_h264_yuv420p(640, 480, false);
+    // // let settings = Settings::preset_h264_yuv420p(640, 480, video_rs::ffmpeg::format::Pixel::RGB24);
+    // let mut v_encoder =
+    //     Encoder::new(Path::new("rainbow.mp4"), settings).expect("failed to create encoder");
+
+    // let duration: Time = Time::from_nth_of_a_second(24);
+    // let mut position = Time::zero();
+    // for rawframe in framebuf {
+
+    //     let mut rgb_frame = Video::empty();
+    //     rgb_frame.set_format(ffmpeg::util::format::pixel::Pixel::RGB24);
+    //     rgb_frame.set_width(width);
+    //     rgb_frame.set_height(height);
+
+    //     for (idx, pixel) in rawframe.iter().enumerate() {
+    //         let gray_value = (pixel / 64) as u8;
+
+    //         rgb_frame.data_mut(0)[idx * 3 + 0] = gray_value; // R
+    //         rgb_frame.data_mut(0)[idx * 3 + 1] = gray_value; // G
+    //         rgb_frame.data_mut(0)[idx * 3 + 2] = gray_value; // B
+    //     }
+
+    //     v_encoder.encode_raw(rgb_frame);
+    //     // encoder
+    //     //     .encode(&rgbframe, &position)
+    //     //     .expect("failed to encode frame");
+
+    //     // Update the current position and add the inter-frame duration to it.
+    //     position = position.aligned_with(&duration).add();
+    // }
+
+    // v_encoder.finish().expect("failed to finish encoder");
+
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    // use opencv::{ core, imgproc, prelude::*, videoio };
+
+    // // MP4V 코덱과 비디오 작성자 초기화
+    // let fourcc = videoio::VideoWriter::fourcc('M' as i8, 'P' as i8, '4' as i8, 'V' as i8)?;
+    // let mut writer = videoio::VideoWriter::new("C:\\output.mp4", fourcc, fps, core::Size::new(width, height), true)?;
+
+    // for frame_data in framebuf.iter() {
+    //     // OpenCV의 Mat 객체를 생성하여 프레임 데이터를 저장
+    //     let mut frame = core::Mat::new_rows_cols_with_default(height, width, core::CV_8UC3, core::Scalar::new(0.0, 0.0, 0.0, 0.0))?;
+
+    //     for (idx, &pixel) in frame_data.iter().enumerate() {
+    //         let gray_value = (pixel / 64) as u8;
+    //         let color = core::Scalar::new(gray_value as f64, gray_value as f64, gray_value as f64, 0.0);
+
+    //         // 각 픽셀의 위치 계산 (단순화된 예제)
+    //         let x = idx % width;
+    //         let y = idx / width;
+
+    //         // 단일 픽셀에 색상 적용
+    //         imgproc::circle(&mut frame, core::Point::new(x as i32, y as i32), 1, color, -1, imgproc::LINE_8, 0)?;
+    //     }
+
+    //     // 프레임을 비디오에 추가
+    //     writer.write(&frame)?;
+    // }
+
+    // print!("saved!!!!\n")
 }
